b94f9c3ea1b060fb111d921df082497f370d3963
zengzengzenghuy <tayzh2799@gmail.com>
2024-03-11 14:52:04 +0800

    remove unused files

---
+++ offchain/src/index.js
@@ -0,0 +1,33 @@
+const { MessageRelayedEventListener, DVNEventListener } = require('./src/EventListener');
+const { HashiDVNSender } = require('./src/HashiDVNSender');
+const { ViemClient } = require('./viemClient');
+const { chainConfig } = require('../config');
+const { gnosis, optimism } = require('viem/chains');
+
+const { createWalletClient, http, publicActions } = require('viem');
+const { privateKeyToAccount } = require('viem/accounts');
+
+async function main(){
+ 
+
+    const sourceChain = "gnosis";
+    const destChain = "optimism";
+
+    const viemClient = new ViemClient(`0x${process.env.DEPLOYER_PRIV_KEY}`);
+
+    const messageRelayedEventListener = new MessageRelayedEventListener(viemClient, sourceChain, destChain );
+    const stopMessageRelay = messageRelayedEventListener.start();
+    const DVNEventListener = new DVNEventListener(viemClient, sourceChain, destChain);
+    const stopDVN = DVNEventListener.start();
+    const hashiDVNSender = new HashiDVNSender(viemClient, destChain);
+    hashiDVNSender.addEvent(messageRelayedEventListener.getMessageIdsAndHash, DVNEventListener.getPackets);
+    hashiDVNSender.start();
+
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+  
+
